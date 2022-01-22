d048bb40b86bfe60daa0f24021ed1b699b72ff22
zhangkun <zhangkun3365@gmail.com>
2022-01-22 17:22:19 +0800

    the loader2

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset
+import torch.nn.functional as F
 #from torch.utils.data import DataLoader
 import numpy as np
 class loader_musk(Dataset):
@@ -22,26 +23,36 @@ class loader_musk(Dataset):
             count2 += 1
         y = data[:,-1]
         name = data[:,0]
-        self.y_data = np.zeros(len(dic))
-        self.y_data = torch.from_numpy(self.y_data)
-        print(self.y_data)
+#        self.y_data = np.zeros(len(dic))
+        self.y_data = torch.zeros(rows,dtype=torch.float32)
+#        print(self.y_data)
         data = data[:,2:-1]
-        data_in = np.zeros((rows,cols-3),dtype=np.float32)
+        data_in = torch.zeros((rows,cols-3),dtype=torch.float32)
+        
         for row in range(rows):
             for col in range(cols-3):
                 data_in[row,col] = float(data[row,col])
+        for i in range(cols-3):
+            data_in[:,i] = (data_in[:,i]-data_in.min())/(data_in[:,i].max()-data_in.min())
+#        print(data_in[0])
 #        data.astype(np.float32)
         for i in dic:
-            self.x_data.append(torch.from_numpy(data_in[dic[i][0]:dic[i][0]+dic[i][1],:]))
+            self.x_data.append(data_in[dic[i][0]:dic[i][0]+dic[i][1],:].clone())
             for j in range(dic[i][0],dic[i][0]+dic[i][1]):
                 if y[j][0] == '1':
                     self.y_data[leng] = 1
             leng += 1
-        print(self.x_data[0])
-        print(self.x_data[1][3])
-        print(self.y_data)
+        self.len = leng
+        self.lengtht = cols-3
+ #       F.normalize(self.x_data,dim=1)
+        print(self.x_data[0].shape)
+        # print(self.x_data[91][2])
+        # print(self.y_data)
+        
     def __getitem__(self, index):
         return self.x_data[index] , self.y_data[index]
     def __len__(self):
         return self.len
+    def __length__(self):
+        return self.lengtht
 dataset = loader_musk('clean1.data')
